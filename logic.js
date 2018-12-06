81cdced46629b92c6e536d321a74a20348b86ba7
JrmSK <jeremy.skornik@gmail.com>
2018-12-06 11:02:42 +0200

    Created a function to generate all the 81 cards

---
+++ logic.js
@@ -0,0 +1,38 @@
+// All specifics for cards creation
+window.SetGame = {};    // maybe go back to var 
+SetGame.colors = ["purple", "green", "red"];
+SetGame.shapes = ["circle", "heart", "diamond"];
+SetGame.fillings = ["solid", "striped", "outlined"];
+SetGame.numbers = [1, 2, 3];
+SetGame.cardsCollection = [];
+
+class Card {
+    constructor(name, color, shape, filling, number) {
+        this.name = name;
+        this.color = color;
+        this.shape = shape;
+        this.filling = filling;
+        this.number = number;
+    }
+};
+
+
+SetGame.generateAllCards = function () {
+    var counter = 0;
+    var name = "";
+    for (var i = 0; i < SetGame.colors.length; i++) {
+        for (var j = 0; j < SetGame.shapes.length; j++) {
+            for (var k = 0; k < SetGame.fillings.length; k++) {
+                for (var l = 0; l < SetGame.numbers.length; l++) {
+                    name = `card${counter}`;
+                    counter++;
+                    SetGame[name] = new Card (name, SetGame.colors[i], SetGame.shapes[j], SetGame.fillings[k], SetGame.numbers[l]);
+                    SetGame.cardsCollection.push(SetGame[name]);
+                }
+            }
+        }
+    }
+}
+
+SetGame.generateAllCards();
+console.log(SetGame.cardsCollection);
