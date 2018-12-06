8dc5036294a758e2ba3c5710fe06684562fa169d
moshe melmaed <moshe.melamed@mail.huji.ac.il>
2018-12-06 10:58:13 +0200

    changed from old shape name to heart

--- logic.js
+++ logic.js
@@ -8,20 +8,20 @@ class Card {
         this.filing = filing;
     }
 }
- SetGame.card1 = new Card("cercle","red",1,"solid");
- SetGame.card2 = new Card("cercle","red",1,"solid");
- SetGame.card3 = new Card("cercle","red",1,"solid");
- SetGame.card4 = new Card("cercle","red",2,"striped");
- SetGame.card5 = new Card("square","purple",2,"striped");
- SetGame.card6 = new Card("square","purple",3,"striped");
- SetGame.card7 = new Card("square","purple",3,"striped");
- SetGame.card8 = new Card("square","purple",2,"outlined");
+ SetGame.card1 = new Card("circle","red",1,"solid");
+ SetGame.card2 = new Card("circle","red",1,"solid");
+ SetGame.card3 = new Card("circle","red",1,"solid");
+ SetGame.card4 = new Card("circle","red",2,"striped");
+ SetGame.card5 = new Card("heart","purple",2,"striped");
+ SetGame.card6 = new Card("heart","purple",3,"striped");
+ SetGame.card7 = new Card("heart","purple",3,"striped");
+ SetGame.card8 = new Card("heart","purple",2,"outlined");
  SetGame.card9 = new Card("diamond","purple",1,"outlined");
  SetGame.card10 = new Card("diamond","purple",2,"outlined");
  SetGame.card11 = new Card("diamond","purple",3,"solid");
  SetGame.card12 = new Card("diamond","purple",3,"solid");
 
-SetGame.shape = ["cercle", "square", "diamond"];
+SetGame.shape = ["circle", "heart", "diamond"];
 SetGame.color = ["purple", "red", "green"];
 SetGame.number = [1, 2, 3];
 SetGame.filing = ["solid", "striped", "outlined"];
