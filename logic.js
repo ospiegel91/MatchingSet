63c3a1993e27db614f12ce2f1d0b0a4478ffaf41
moshe melmaed <moshe.melamed@mail.huji.ac.il>
2018-12-06 11:45:57 +0200

    implemetation of isSet function

--- logic.js
+++ logic.js
@@ -1,38 +1,3 @@
-// // All specifics for cards creation
-// window.SetGame = {};    // maybe go back to var 
-// SetGame.colors = ["purple", "green", "red"];
-// SetGame.shapes = ["circle", "heart", "diamond"];
-// SetGame.fillings = ["solid", "striped", "outlined"];
-// SetGame.numbers = [1, 2, 3];
-// SetGame.cardsCollection = [];
-
-// class Card {
-//     constructor(name, color, shape, filling, number) {
-//         this.name = name;
-//         this.color = color;
-//         this.shape = shape;
-//         this.filling = filling;
-//         this.number = number;
-//     }
-// };
-
-
-// SetGame.generateAllCards = function () {
-//     var counter = 0;
-//     var name = "";
-//     for (var i = 0; i < SetGame.colors.length; i++) {
-//         for (var j = 0; j < SetGame.shapes.length; j++) {
-//             for (var k = 0; k < SetGame.fillings.length; k++) {
-//                 for (var l = 0; l < SetGame.numbers.length; l++) {
-//                     name = `card${counter}`;
-//                     counter++;
-//                     SetGame[name] = new Card (name, SetGame.colors[i], SetGame.shapes[j], SetGame.fillings[k], SetGame.numbers[l]);
-//                     SetGame.cardsCollection.push(SetGame[name]);
-//                 }
-//             }
-//         }
-//     }
-// }
 window.SetGame = {}
 
 class Card {
@@ -61,3 +26,22 @@ SetGame.color = ["purple", "red", "green"];
 SetGame.number = [1, 2, 3];
 SetGame.filing = ["solid", "striped", "outlined"];
 
+SetGame.isMatch = function(category1,category2,category3){
+    if(category1 === category2 && category2 === category3 && category1 === category3 ||
+        category1 !== category2 && category1 !== category3 && category2 !== category3){
+            return true;
+        } else {
+            return false;
+        }
+}
+SetGame.isSet = function (card1, card2, card3){
+    var isShape = SetGame.isMatch(card1.shape, card2.shape, card3.shape);
+    var isColor = SetGame.isMatch(card1.color, card2.color, card3.color);
+    var isNumber = SetGame.isMatch(card1.number, card2.number, card3.number);
+    var isFilling = SetGame.isMatch(card1.filling, card2.filling, card3.filling);
+    if (isShape && isColor && isNumber && isFilling){
+        return true;
+    } else {
+        return false;
+    }
+}
