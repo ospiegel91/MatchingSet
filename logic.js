950ff5a1dcfdc9e777b75a8c8f5e7550ac8259d6
JrmSK <jeremy.skornik@gmail.com>
2018-12-06 11:27:54 +0200

    resolved an issue that broke code. Now only hard coded is used

--- logic.js
+++ logic.js
@@ -1,40 +1,38 @@
-<<<<<<< HEAD
-// All specifics for cards creation
-window.SetGame = {};    // maybe go back to var 
-SetGame.colors = ["purple", "green", "red"];
-SetGame.shapes = ["circle", "heart", "diamond"];
-SetGame.fillings = ["solid", "striped", "outlined"];
-SetGame.numbers = [1, 2, 3];
-SetGame.cardsCollection = [];
+// // All specifics for cards creation
+// window.SetGame = {};    // maybe go back to var 
+// SetGame.colors = ["purple", "green", "red"];
+// SetGame.shapes = ["circle", "heart", "diamond"];
+// SetGame.fillings = ["solid", "striped", "outlined"];
+// SetGame.numbers = [1, 2, 3];
+// SetGame.cardsCollection = [];
 
-class Card {
-    constructor(name, color, shape, filling, number) {
-        this.name = name;
-        this.color = color;
-        this.shape = shape;
-        this.filling = filling;
-        this.number = number;
-    }
-};
+// class Card {
+//     constructor(name, color, shape, filling, number) {
+//         this.name = name;
+//         this.color = color;
+//         this.shape = shape;
+//         this.filling = filling;
+//         this.number = number;
+//     }
+// };
 
 
-SetGame.generateAllCards = function () {
-    var counter = 0;
-    var name = "";
-    for (var i = 0; i < SetGame.colors.length; i++) {
-        for (var j = 0; j < SetGame.shapes.length; j++) {
-            for (var k = 0; k < SetGame.fillings.length; k++) {
-                for (var l = 0; l < SetGame.numbers.length; l++) {
-                    name = `card${counter}`;
-                    counter++;
-                    SetGame[name] = new Card (name, SetGame.colors[i], SetGame.shapes[j], SetGame.fillings[k], SetGame.numbers[l]);
-                    SetGame.cardsCollection.push(SetGame[name]);
-                }
-            }
-        }
-    }
-}
-=======
+// SetGame.generateAllCards = function () {
+//     var counter = 0;
+//     var name = "";
+//     for (var i = 0; i < SetGame.colors.length; i++) {
+//         for (var j = 0; j < SetGame.shapes.length; j++) {
+//             for (var k = 0; k < SetGame.fillings.length; k++) {
+//                 for (var l = 0; l < SetGame.numbers.length; l++) {
+//                     name = `card${counter}`;
+//                     counter++;
+//                     SetGame[name] = new Card (name, SetGame.colors[i], SetGame.shapes[j], SetGame.fillings[k], SetGame.numbers[l]);
+//                     SetGame.cardsCollection.push(SetGame[name]);
+//                 }
+//             }
+//         }
+//     }
+// }
 window.SetGame = {}
 
 class Card {
@@ -63,4 +61,3 @@ SetGame.color = ["purple", "red", "green"];
 SetGame.number = [1, 2, 3];
 SetGame.filing = ["solid", "striped", "outlined"];
 
->>>>>>> a168d5c8a752881d603e1ba507214559a0e15096
